6ff06ad4ca8d677da1f5234b8a8483b760670efd
knightriderisback <knight.rider.is.back@gmail.com>
2026-08-11 20:04:40 +0530

    Exact Matang PNG logo (as-is, no background) embedded

--- lib/logo-data.ts
+++ lib/logo-data.ts
@@ -1,6 +1,1 @@
-import { P0 } from "./logo-part-0";
-import { P1 } from "./logo-part-1";
-import { P2 } from "./logo-part-2";
-import { P3 } from "./logo-part-3";
-
-export const LOGO_SRC = "data:image/png;base64," + P0 + P1 + P2 + P3;
+PLACEHOLDER_WILL_REPLACE
